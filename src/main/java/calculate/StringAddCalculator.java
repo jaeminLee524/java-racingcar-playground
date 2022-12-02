6673e3d3fd6b77af244949b92b37dab801302b68
이재민 <rkwoals524@hanmail.net>
2022-12-02 22:59:33 +0900

    string add calculator

--- src/main/java/calculate/StringAddCalculator.java
+++ src/main/java/calculate/StringAddCalculator.java
@@ -1,2 +1,23 @@
-package calculate;public class StringAddCalculator {
+package calculate;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String param) {
+        if (param == null || param.isEmpty()) {
+            return 0;
+        }
+        if (param.length() == 1) {
+            return 1;
+        }
+        if (param.contains(",")) {
+            String[] splitParam = param.split(",");
+            int sum = 0;
+            for (String splitData : splitParam) {
+                sum += Integer.parseInt(splitData);
+            }
+            return sum;
+        }
+
+        return Integer.parseInt(param);
+    }
 }
