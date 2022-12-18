ebbdd6e7482de62c9247ca128dbf13088fd2bc24
이재민 <rkwoals524@hanmail.net>
2022-12-18 23:14:51 +0900

    극단적인 refator -> refatoring 시야 넓히기

--- src/main/java/calculate/StringAddCalculator.java
+++ src/main/java/calculate/StringAddCalculator.java
@@ -1,8 +1,14 @@
 package calculate;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
-    public static int splitAndSum(String text) {
+    public static final String CUSTOM_REGEX = "//(.)\n(.*)";
+    public static final String COMMA_COLONS_RㄴEGEX = ",|:";
+
+    public int splitAndSum(String text) {
         if (isBlank(text)) {
             return 0;
         }
@@ -10,12 +16,16 @@ public class StringAddCalculator {
         return add(toInts(split(text)));
     }
 
-    private static String[] split(String text) {
-        String[] values = text.split(",");
-        return values;
+    private String[] split(String text) {
+        Matcher matcher = Pattern.compile(CUSTOM_REGEX).matcher(text);
+        if (matcher.find()) {
+            return matcher.group(2).split(matcher.group(1));
+        }
+
+        return text.split(COMMA_COLONS_RㄴEGEX);
     }
 
-    private static int add(int[] numbers) {
+    private int add(int[] numbers) {
         int sum = 0;
         for (int number : numbers) {
             sum += number;
@@ -24,16 +34,25 @@ public class StringAddCalculator {
         return sum;
     }
 
-    private static int[] toInts(String[] values) {
+    private int[] toInts(String[] values) {
         int[] intArray = new int[values.length];
         for (int i = 0; i < values.length; i++) {
-            intArray[i] = Integer.parseInt(values[i]);
+            intArray[i] = getPositiveNumber(values[i]);
         }
 
         return intArray;
     }
 
-    private static boolean isBlank(String text) {
+    private int getPositiveNumber(String text) {
+        int number = Integer.parseInt(text);
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+
+        return number;
+    }
+
+    private boolean isBlank(String text) {
         if (text == null || text.isEmpty()) {
             return true;
         }
